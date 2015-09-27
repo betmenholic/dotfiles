8bce4f990836e8ba0920db10b1c65818aa4d538a
betmenholic <betmen@openmailbox.org>
2015-09-27 20:28:18 +0700

    add my scripts stuff

---
+++ bin/colors.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "
+\033[30mBlack      \033[0m[30]    \033[1;30mbold \033[0m[1]    \033[2;30mdark \033[0m[2]    \033[7;30mreverse\033[0m [7]    \033[40mbackground\033[0m [40]
+\033[31mRed        \033[0m[31]    \033[1;31mbold \033[0m *     \033[2;31mdark \033[0m *     \033[7;31mreverse\033[0m  *     \033[41mbackground\033[0m [41]
+\033[32mGreen      \033[0m[32]    \033[1;32mbold \033[0m *     \033[2;32mdark \033[0m *     \033[7;32mreverse\033[0m  *     \033[42mbackground\033[0m [42]
+\033[33mYellow     \033[0m[33]    \033[1;33mbold \033[0m *     \033[2;33mdark \033[0m *     \033[7;33mreverse\033[0m  *     \033[43mbackground\033[0m [43]
+\033[34mBlue       \033[0m[34]    \033[1;34mbold \033[0m *     \033[2;34mdark \033[0m *     \033[7;34mreverse\033[0m  *     \033[44mbackground\033[0m [44]
+\033[35mMagenta    \033[0m[35]    \033[1;35mbold \033[0m *     \033[2;35mdark \033[0m *     \033[7;35mreverse\033[0m  *     \033[45mbackground\033[0m [45]
+\033[36mCyan       \033[0m[36]    \033[1;36mbold \033[0m *     \033[2;36mdark \033[0m *     \033[7;36mreverse\033[0m  *     \033[46mbackground\033[0m [46]
+\033[37mWhite      \033[0m[37]    \033[1;37mbold \033[0m *     \033[2;37mdark \033[0m *     \033[7;37mreverse\033[0m  *     \033[47mbackground\033[0m [47]
+
+\033[39mForeground \033[0m[39]                                           \033[49mBackground\033[0m [49]
+"
+
